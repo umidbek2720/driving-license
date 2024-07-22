9755fdc787549dbb605ed616b01a5286ab4f21bc
umidbek2720 <umidbekruzmetov84@gmail.com>
2024-07-22 10:15:46 +0500

    contants has been fixed

--- app.js
+++ app.js
@@ -12,10 +12,10 @@
 
 // Foydalanuvchidan yoshini va provasi borligini so'raymiz, shundan keyin mashini haydashga ruxsat beramiz yoki 10 yilga mashina haydash huquqidan chetlashtiriladi!!!
 
-const yosh = +prompt("Yoshingizni kiriting: ");
-const result = confirm("Sizda haydovchilik guvohnomasi bormi?");
+const AGE = +prompt("Yoshingizni kiriting: ");
+const HAVELICENSE = confirm("Sizda haydovchilik guvohnomasi bormi?");
 
-if(yosh >= 18 && result) alert("Yo'lingizni davom ettiring, hurmatli haydovchi!!! 👮");
+if(AGE >= 18 && HAVELICENSE) alert("Yo'lingizni davom ettiring, hurmatli haydovchi!!! 👮");
 else alert("tabriklaymiz, siz 10 yilga mashina haydash huquqidan chetlatildingiz 🎉🎉🎉");
 
 // git - vaqt mashinasi, local server
