69e2666a5361aaca3637654e801593b6816d9d6d
umidbek2720 <umidbekruzmetov84@gmail.com>
2024-07-22 10:07:16 +0500

    HTML file has been created and has been done task

---
+++ app.js
@@ -0,0 +1,23 @@
+// let y = 81;
+// let z = "22";
+// let b = 45.4;
+// let a = 22 > 45;
+// let x = "Asssalomu alaykum";
+
+// alert(typeof x);
+// alert(typeof y);
+// alert(typeof z);
+// alert(typeof a);
+// alert(typeof b);
+
+// Foydalanuvchidan yoshini va provasi borligini so'raymiz, shundan keyin mashini haydashga ruxsat beramiz yoki 10 yilga mashina haydash huquqidan chetlashtiriladi!!!
+
+const yosh = +prompt("Yoshingizni kiriting: ");
+const result = confirm("Sizda haydovchilik guvohnomasi bormi?");
+
+if(yosh >= 18 && result) alert("Yo'lingizni davom ettiring, hurmatli haydovchi!!! 👮");
+else alert("tabriklaymiz, siz 10 yilga mashina haydash huquqidan chetlatildingiz 🎉🎉🎉");
+
+// git - vaqt mashinasi, local server
+// github - global server
+// ctrl tilda ~
